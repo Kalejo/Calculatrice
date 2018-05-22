45a5ccf7e6894b8540f4e6ec1057e2daded64cfd
Alexandro <alejo_alvarez67@hotmail.com>
2018-05-22 18:46:23 +0200

    test

---
+++ js/main.js
@@ -0,0 +1,24 @@
+ function Calculator()
+{
+    that                = this,
+    this.field  = "input#number",
+    this.button = "#body .buttons",
+    this.init   = false,
+
+    this.run = function()
+    {
+        $(this.button).click(function() {
+            var value = $(this).html();
+
+            if (that.init == false)
+            {
+                $(that.field).val("");
+                that.init = true;
+            }
+
+            if (value != "=")
+                $(that.field).val($(that.field).val() + value);
+
+            that.dispatcher(value);
+        });
+    },
